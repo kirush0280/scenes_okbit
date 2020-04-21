cd6cc5bda5e727b0fb329bfa8c898d2f25f6d1f1
Zaitsev-ap <54988721+Zaitsev-ap@users.noreply.github.com>
2020-04-21 15:00:01 +0300

    Update link_show_usual.php

--- modules/scenes_okbit/elements/link_show_usual.php
+++ modules/scenes_okbit/elements/link_show_usual.php
@@ -1,4 +1,4 @@
-<a href="#" onclick="block_none(); show_center_{{CENTER_SHOW}}(); show_right_{{RIGHT_SHOW}}();">
+<a href="#" onclick="block_none(); show_center_{{CENTER_SHOW}}();">
 	<div class="left-link">
 		<div class="link-ico link-ico-{{ICO}}">
 		</div><span>{{TITLE}}</span>
