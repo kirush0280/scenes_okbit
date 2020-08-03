a0b140989b7d350d4ce6fa5beeb0500fae8767eb
Alexander Bezzubov <bezzubov@gmail.com>
2020-08-03 16:11:10 +0300

    Добавлена кликабельная ссылка на значения. Открывается история значений

--- modules/scenes_okbit/elements/info_usual.php
+++ modules/scenes_okbit/elements/info_usual.php
@@ -1,7 +1,7 @@
 	<div class="center-in">
 		<div class="link-ico info-ico-{{ICO}}"></div>
 		<span>{{TITLE}}</span>
-		<div class="info-s">%{{OBJECT}}.{{PROPERTY}}% {{CENTER_SHOW}}</div>
+		<div class="info-s"><a href="/pChart/?p={{LINKED_PROPERTY}}&op=log&type=7d&subop=7d">%{{OBJECT}}.{{PROPERTY}}%</a> {{CENTER_SHOW}}</div>
 	</div>
 
 	
